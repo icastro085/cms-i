c43bb49d59fadb1c592868e0be97d3dcc0901b05
Ivanildo de Castro <ivanildo.decastro085@gmail.com>
2017-11-05 19:41:53 -0400

    Adding HR plugin

--- src/components/Content/Edit.js
+++ src/components/Content/Edit.js
@@ -35,11 +35,12 @@ export default class Form extends Component {
             theme: 'modern',
             statusbar : false,
             plugins: [
+              'hr',
               'advlist autolink lists link image charmap print preview anchor textcolor',
               'searchreplace visualblocks code fullscreen',
               'insertdatetime media table contextmenu paste code help'
             ],
-            toolbar: 'insert | undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help'
+            toolbar: 'hr insert | undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help'
           }}
           onChange={this.onChangeText}
         />
